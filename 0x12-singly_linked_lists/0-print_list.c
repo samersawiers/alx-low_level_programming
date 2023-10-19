97071c65ff5e171621ccbcb8cac816c29417740e
samer <samer@gmail.com>
2023-10-19 15:26:48 -0700

    singly_linked_lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,7 +8,7 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t n = 0;
+	size_t nodes = 0;
 	while (h)
 	{
 		if (h->str == null)
@@ -17,9 +17,9 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%d] %s\n", h->len, h->str);
 
-		n++;
+		nodes++;
 		h = h->next;
 	}
 
-	return (n);
+	return (nodes);
 }
