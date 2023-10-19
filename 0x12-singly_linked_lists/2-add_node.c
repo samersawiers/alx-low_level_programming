35c40e494f70bc7ee1ec842da24238b334e62268
samer <samer@gmail.com>
2023-10-19 16:02:43 -0700

    singly_linked_lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,8 +3,8 @@
 
 /**
  * add_node - Adds a new node at the beginning of a linked list list_t list.
- * @str: The string to be added to the list_t list. 
  * @head: A pointer to the head of the list_t list.
+ * @str: The string to be added to the list_t list. 
  * 
  * Return: If function fails - NULL. , Otherwise - the address of the new element.
  */
