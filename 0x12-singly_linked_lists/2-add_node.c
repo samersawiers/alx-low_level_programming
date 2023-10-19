74d5b2c2473c731b666147aac3e264e047940ac5
samer <samer@gmail.com>
2023-10-19 16:05:10 -0700

    singly_linked_lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,14 +3,14 @@
 
 /**
  * add_node - Adds a new node at the beginning of a linked list list_t list.
- * @head: A pointer to the head of the list_t list.
  * @str: The string to be added to the list_t list. 
+ *@head: A pointer to the head of the list_t list.      
  * 
  * Return: If function fails - NULL. , Otherwise - the address of the new element.
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	char *dupl;
+	char *dup;
 	int len;
 	list_t *new;
 
@@ -18,8 +18,8 @@ list_t *add_node(list_t **head, const char *str)
 	if (new == NULL)
 		return (NULL);
 
-	dupl = strdup(str);
-	if (dupl == NULL)
+	dup = strdup(str);
+	if (dup == NULL)
 	{
 		free(new);
 		return (NULL);
@@ -28,7 +28,7 @@ list_t *add_node(list_t **head, const char *str)
 	for (len = 0; str[len];)
 		len++;
 
-	new->str = dupl;
+	new->str = dup;
 	new->len = len;
 	new->next = *head;
 
